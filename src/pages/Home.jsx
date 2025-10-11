2e0ee06acc05014220a94a8c5a2f5fb20dd8dce9
Javier nc. <ja.niloc@duocuc.cl>
2025-10-11 16:40:27 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import { Container } from 'react-bootstrap';
 function Home() {
     return (
       <Container className="my-5">
+        <h1>Página de inicio</h1>
         <p>Bienvenidos a nuestro sitio web.</p>
 
       </Container>
